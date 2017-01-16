08bbf628ad85a6b0325cac97071b03222ee74de1
livos <lvivancos@gmail.com>
2017-01-16 10:35:20 +0700

    Some stuffs fixed

--- app/src/main/java/com/example/epubreader/MainActivity.java
+++ app/src/main/java/com/example/epubreader/MainActivity.java
@@ -1,80 +1,106 @@
 package com.example.epubreader;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.MediaType;
 import nl.siegmann.epublib.domain.Resource;
 import nl.siegmann.epublib.domain.Resources;
 import nl.siegmann.epublib.domain.Spine;
 import nl.siegmann.epublib.domain.SpineReference;
 import nl.siegmann.epublib.domain.TOCReference;
 import nl.siegmann.epublib.epub.EpubReader;
+import nl.siegmann.epublib.service.MediatypeService;
 
 public class MainActivity extends AppCompatActivity {
     private static final String EBOOK_FILE_PATH = "/sdcard/Download/le_petit_prince_fr.epub";
     private static String LOG_TAG = MainActivity.class.getSimpleName();
 
+    //=== Read ebook option 1 (View into a WebView)
+    private void viewBook(Book book) {
+        InputStream inputStream = null;
+
+        WebView wv = (WebView) findViewById(R.id.wv_text);
+
+        Spine spine = new Spine(book.getTableOfContents());
+
+        StringBuilder sb = new StringBuilder();
+        for (SpineReference bookSection : spine.getSpineReferences()) {
+            Resource res = bookSection.getResource();
+            try {
+                inputStream = res.getInputStream();
+
+                BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = r.readLine()) != null) {
+                    sb.append(line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        wv.loadDataWithBaseURL(null, sb.toString(), "text/html", "utf-8", null);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        WebView wv = (WebView)findViewById(R.id.wv_text);
         EpubReader epubReader = new EpubReader();
+        FileInputStream fileInputStream = null;
 
         try {
-            Book book = epubReader.readEpub(new FileInputStream(EBOOK_FILE_PATH));
+            fileInputStream = new FileInputStream(EBOOK_FILE_PATH);
+            Book book = epubReader.readEpub(fileInputStream);
+            viewBook(book);
 
-            Spine spine = new Spine(book.getTableOfContents());
-
-            StringBuilder sb = new StringBuilder();
-            for (SpineReference bookSection : spine.getSpineReferences()) {
-                Resource res = bookSection.getResource();
-                try {
-                    InputStream is = res.getInputStream();
-
-                    BufferedReader r = new BufferedReader(new InputStreamReader(is));
-
-                    String line = null;
-
-                    while ((line = r.readLine()) != null) {
-
-                        sb.append(line);
-                    }
-                    is.close();
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            Log.d(LOG_TAG,sb.toString());
-
-
-            wv.loadDataWithBaseURL( null,sb.toString(), "text/html", "utf-8",null);
-
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-
-
-//=== Read ebook option 2
-        // TextView txtText = (TextView) findViewById(R.id.txt_text);
+    }
+//=== Read ebook option 2 (View into a TextView)
+// TextView txtText = (TextView) findViewById(R.id.txt_text);
 //        try {
 //            Book book = epubReader.readEpub(new FileInputStream(EBOOK_FILE_PATH));
 //
@@ -102,26 +128,4 @@ public class MainActivity extends AppCompatActivity {
 //            e.printStackTrace();
 //        }
 
-//=== READ ebook option 1
-//        TextView txtText = (TextView) findViewById(R.id.txt_text);
-//        try {
-//            book = epubReader.readEpub(new FileInputStream(EBOOK_FILE_PATH));
-//            List<TOCReference> tocReferences = book.getTableOfContents().getTocReferences();
-//            for(TOCReference tocReference : tocReferences) {
-//                InputStream is = tocReference.getResource().getInputStream();
-//                BufferedReader r = new BufferedReader(new InputStreamReader(is));
-//
-//                String line;
-//                TextView txtText = (TextView) findViewById(R.id.txt_text);
-//                while ((line = r.readLine()) != null) {
-//                    if(line.trim() != "") {
-//                        line = Html.fromHtml(line).toString();
-//                        txtText.setText(txtText.getText() + line);
-//                    }
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-    }
 }
